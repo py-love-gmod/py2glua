26f6fef2ed166a0a4b733d203d0d7f345ac15afe
themanyfaceddemon <neko-d-05@mail.ru>
2025-10-08 16:59:28 +0300

    add base compiler (WIP)

---
+++ py2glua/compiler.py
@@ -0,0 +1,23 @@
+import logging
+from pathlib import Path
+
+logger = logging.getLogger("py2glua")
+
+
+# TODO: Доделать
+class Compiler:
+    @classmethod
+    def rgrob_files(cls, source: Path) -> list[Path] | None:
+        if not source.exists():
+            logger.warning(f"[compiler] Путь {source} не существует")
+            return None
+
+        if not source.is_dir():
+            logger.warning(f"[compiler] Путь {source} не является папкой")
+            return None
+
+        list_of_files: list[Path] = []
+        for path in source.rglob(".py"):
+            list_of_files.append(path)
+
+        return list_of_files
