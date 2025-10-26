6fc453a4a3accb72056b63d4f35591cf79851824
themanyfaceddemon <neko-d-05@mail.ru>
2025-10-27 01:59:58 +0300

    owo

--- py2glua/normalizer/base_normalizer.py
+++
@@ -1,9 +0,0 @@
-from ..ir.ir_base import File
-
-
-class BaseNormalizer:
-    priority: int = 0
-
-    @classmethod
-    def run(cls, file: File) -> None:
-        raise NotImplementedError
---
+++ tests/test_owo.py
@@ -0,0 +1,2 @@
+def test_owo():
+    print("owo")
