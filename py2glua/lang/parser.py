f98fe14d36646c0f482f0966617566ced760c5f8
themanyfaceddemon <neko-d-05@mail.ru>
2025-10-27 16:18:05 +0300

    parser

--- py2glua/lang/parser.py
+++ py2glua/lang/parser.py
@@ -1,5 +1,6 @@
 import ast
 import tokenize
+from enum import Enum, auto
 from io import BytesIO
 
 
@@ -23,6 +24,19 @@ class _TokenStream:
 
         return tok
 
+    def match(self, type_=None, value=None):
+        tok = self.peek()
+        if tok is None:
+            return False
+
+        if type_ is not None and tok.type != type_:
+            return False
+
+        if value is not None and tok.string != value:
+            return False
+
+        return True
+
     def eof(self) -> bool:
         return self.pos >= len(self.tokens)
 
@@ -31,27 +45,72 @@ class _TokenStream:
 
 
 # region RawLex
+class RawNodeKind(Enum):
+    IMPORT = auto()
+    FUNCTION = auto()
+    OTHER = auto()
+
+
 class _BaseRawLex:
-    pass
+    def __init__(
+        self,
+        kind: RawNodeKind,
+        list_of_tokens: list[tokenize.TokenInfo],
+    ) -> None:
+        self.kind = kind
+        self.tokens = list_of_tokens
 
 
-class _RawLexStream:
-    pass
+class RawNode(_BaseRawLex):
+    def __init__(self, kind, tokens):
+        self.kind = kind
+        self.tokens = tokens
 
 
 # endregion
 
 
 class Parser:
+    # region tokens
     @staticmethod
     def _constract_tokens(source: str) -> _TokenStream:
         ast.parse(source)
 
         token_list = []
-        for tok in tokenize.tokenize(BytesIO(source.encode("utf-8")).readline):
-            token_list.append(tok)
+        for token in tokenize.tokenize(BytesIO(source.encode("utf-8")).readline):
+            token_list.append(token)
 
         return _TokenStream(token_list)
 
-    @staticmethod
-    def _constract_raw_lex(token_stream: _TokenStream) -> _RawLexStream: ...
+    # endregion
+
+    # region lex
+    @classmethod
+    def _constract_raw_lex(cls, token_stream: _TokenStream):
+        nodes = []
+
+        while not token_stream.eof():
+            # region import
+            if token_stream.match(value="import") or token_stream.match(value="from"):
+                nodes.append(cls._build_raw_import(token_stream))
+                continue
+            # endregion
+
+        return nodes
+
+    @classmethod
+    def _build_raw_import(cls, token_stream: _TokenStream):
+        tokens = []
+
+        while not token_stream.eof():
+            tok = token_stream.advance()
+            if tok is None:
+                break
+
+            tokens.append(tok)
+            if tok.type in (tokenize.NEWLINE, tokenize.NL):
+                break
+
+        return RawNode(RawNodeKind.IMPORT, tokens)
+
+    # endregion
