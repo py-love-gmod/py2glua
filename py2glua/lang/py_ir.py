46d93859cbca19ca12db86b0380f2f6d6169de8a
themanyfaceddemon <neko-d-05@mail.ru>
2025-10-30 16:18:31 +0300

    fuck this

--- py2glua/lang/py_ir.py
+++ py2glua/lang/py_ir.py
@@ -1,6 +1,42 @@
 from dataclasses import dataclass
+from enum import Enum, auto
 
 
 @dataclass
 class PyIRNode:
-    pass
+    line: int
+    offset: int
+
+    def walk(self):
+        raise NotImplementedError
+
+
+# region file
+@dataclass
+class PyIRFile(PyIRNode):
+    body: list[PyIRNode]
+
+    def walk(self):
+        yield self
+        for node in self.body:
+            yield node
+
+
+# endregion
+
+
+# region import
+class ImportType(Enum):
+    EXTERNAL = auto()
+    PYTHON_STD = auto()
+    INTERNAL = auto()
+    UNKNOWN = auto()
+
+
+@dataclass
+class PyIRImport(PyIRNode):
+    type: ImportType
+    moduel: str
+
+
+# endregion
--- py2glua/lang/py_ir_builder.py
+++ py2glua/lang/py_ir_builder.py
@@ -1,2 +1,11 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from .parser import Parser, RawNode, RawNodeKind
+from .py_ir import PyIRFile, PyIRNode
+
+
 class PyIRBuilder:
-    pass
+    @classmethod
+    def build_ir(cls, path_to_file: Path):
+        list_of_raw_lex = Parser.parse(path_to_file.read_text(encoding="utf-8-sig"))
