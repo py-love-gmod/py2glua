f63aea9a40025bf5b4dec95fd7891ac0e86e3cbe
themanyfaceddemon <neko-d-05@mail.ru>
2025-11-21 20:29:17 +0300

    fix code for init in glua

--- py2glua/glua/__init__.py
+++ py2glua/glua/__init__.py
@@ -1,5 +1,13 @@
 from .directive_compiler import CompilerDirective
 from .globals import Global
+from .gmod_files import GmodBasePath
 from .realm import Realm
+from .unsafe import Unsafe
 
-__all__ = ["CompilerDirective", "Global", "Realm"]
+__all__ = [
+    "CompilerDirective",
+    "Global",
+    "GmodBasePath",
+    "Realm",
+    "Unsafe",
+]
