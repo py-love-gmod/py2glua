a38c27ad62dddee4d4063897b815f69f409cad23
themanyfaceddemon <gastermini357@gmail.com>
2026-01-21 13:45:56 +0300

    fix docs

--- py2glua/glua/core/globals.py
+++ py2glua/glua/core/globals.py
@@ -7,15 +7,37 @@ from .compiler_directive import CompilerDirective
 @CompilerDirective.no_compile()
 class Global:
     @staticmethod
-    def var() -> Any:
-        """Помечает переменную как внешне глобально доступную."""
+    def create_external_var() -> Any:
+        """
+        Компиляторная директива.
+
+        Помечает переменную как внешне доступную для использования за пределами аддона.
+
+        НЕ делает символ глобальным во время выполнения.
+        """
         pass
 
     @staticmethod
-    def mark() -> Callable:
-        """Помечает класс/функцию/метод как внешне глобально доступную."""
+    def create_external_def() -> Callable:
+        """
+        Компиляторный декоратор.
+
+        Помечает функцию, метод как внешне доступный символ.
+
+        НЕ делает символ глобальным во время выполнения.
+        """
 
         def decorator(fn):
             return fn
 
         return decorator
+
+    @staticmethod
+    def use(ref: Any) -> Any:
+        """
+        Компиляторная ссылка на символ.
+
+        Явно указывает компилятору, что данный символ
+        должен быть найден и использован вне текущего скоупа.
+        """
+        pass
